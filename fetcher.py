07f5deeeb28c17c3fc5d77182f8be61703ddeb62
Manuel Graf <grafman2@students.zhaw.ch>
2024-01-11 08:05:25 +0100

    refactoring

--- fetcher.py
+++ fetcher.py
@@ -4,14 +4,28 @@ import psycopg2
 from dotenv import load_dotenv
 from datetime import datetime
 from selenium import webdriver
-load_dotenv()
-chrome = webdriver.Chrome()
-chrome.get("https://www.taminatherme.ch")
-source = chrome.page_source
-print(source)
-result = re.search(r"<span class=\"block font-bold\">([0-9]+)%</span>", source)
-occupancy = int(result.group(1))
 
+load_dotenv()
+# TODO: installation of python-dotenv, psycopg2, selenium
+
+def loadSource():
+    chrome = webdriver.Chrome()
+    chrome.get("https://www.taminatherme.ch")
+    return chrome.page_source 
+
+source = loadSource()
+
+def getOccupancy(sourceString):
+    result = re.search(r"<span class=\"block font-bold\">([0-9]+)%</span>", sourceString)
+    if result.group(1):
+        return int(result.group(1))
+    else:
+        return -1
+
+occupancy = getOccupancy(source)
+if (occupancy < 0):
+    print("Could not find occupancy on Website")
+    exit(0)
 
 def writeNewValueIntoDataBase(timestamp, occupancy):
     DB_USER = os.getenv("DB_USER")
@@ -32,6 +46,7 @@ def writeNewValueIntoDataBase(timestamp, occupancy):
                 """)
     cursor.execute(f"INSERT INTO occupancy(timestamp, occupancy) VALUES({timestamp},{occupancy})");
     connection.commit()
+    # TODO: Error handling
 
 
 writeNewValueIntoDataBase(f"'{datetime.now()}'", occupancy)
