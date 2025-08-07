40bd8f0de468f39025f0322a122f567114f2548d
grab777 <grab777@gmail.com>
2025-08-07 10:42:42 +0200

    fix docker compose, timezone, env variables

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,18 @@
 FROM ubuntu
+ENV TZ="Europe/Zurich"
 WORKDIR /
-COPY ./fetcher.py .
-COPY ./requirements.txt .
 
 RUN apt update
 RUN apt upgrade -y
 RUN apt install -y python3-pip python3-requests cron python3-venv
-# TODO: pyppeteer does not work correctly, change to playwright
-# dependencies for puppeteer
-# RUN apt install -y ca-certificates fonts-liberation libatk-bridge2.0-0 libatk1.0-0 \
-                #    libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 \
-                #    libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 \
-                #    libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 \
-                #    libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget \
-                #    xdg-utils
 
+COPY ./requirements.txt .
 ENV VIRTUAL_ENV=/opt/venv
 RUN python3 -m venv $VIRTUAL_ENV
 ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 RUN pip install -r requirements.txt
 RUN playwright install
+RUN playwright install-deps
 
 ENV DB_USER=postgres
 ENV DB_PORT=5432
@@ -28,10 +21,11 @@ ENV DB_DATABASE_NAME=postgres
 
 # add cronjob
 COPY ./cron /etc/cron.d/cron
+COPY ./fetcher.py .
 
 RUN chmod 0644 /etc/cron.d/cron
 RUN crontab /etc/cron.d/cron
 RUN chmod +x /fetcher.py
-RUN touch /cron_task.log
+RUN touch /fetcher.log
 SHELL ["/bin/bash", "-c"]
-CMD cron -f && tail -f cron_task.log
+CMD cron -f && tail -f fetcher.log
--- fetcher.py
+++ fetcher.py
@@ -6,16 +6,17 @@ from playwright.sync_api import sync_playwright
 
 from dotenv import load_dotenv
 from datetime import datetime
-import asyncio
 import time
 
 load_dotenv()
 
-debug = True
+debug = True if os.getenv("DEBUG") else False
+logFilePath = os.getenv("LOG_FILE_PATH") or "/fetcher.log"
+
 
 def loadSource():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         # browser = await launch(headless=True,args=['--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--disable-software-rasterizer', '--disable-setuid-sandbox'])
         page = browser.new_page()
         url = os.getenv("URL_TO_CRAWL") or "https://www.taminatherme.ch"
@@ -25,59 +26,76 @@ def loadSource():
         title = page.title()
         html_content = page.content()
         browser.close()
-        if (debug):
+        if debug:
             print("loaded website:", title, url)
-            with open("content.html", "w", encoding="utf-8") as writer:
+            with open("./temp/content.html", "w", encoding="utf-8") as writer:
                 writer.write(html_content)
                 writer.close()
         return html_content
 
 
 def getOccupancy(sourceString):
-    customRegex = os.getenv("CUSTOM_REGEX") or r"<span class=\"block font-bold\">([0-9]+)%</span>"
+    customRegex = (
+        os.getenv("CUSTOM_REGEX") or r"<span class=\"block font-bold\">([0-9]+)%</span>"
+    )
     result = re.search(customRegex, sourceString)
-    if (debug):
+    if debug:
         print("regex used:", customRegex)
         print(f"Regex result\n{result}")
-    if result and result.group(1) and int(result.group(1)) >= 0 and int(result.group(1)) <= 100:
+    if (
+        result
+        and result.group(1)
+        and int(result.group(1)) >= 0
+        and int(result.group(1)) <= 100
+    ):
         print("Occupancy: " + result.group(1))
         return int(result.group(1))
     else:
         print("Could not find occupancy on Website")
         exit(0)
 
+
 def writeNewValueIntoDataBase(timestamp, occupancy):
     DB_USER = os.getenv("DB_USER")
-    pwFileLocation = os.getenv("PW_FILE_LOCATION") or "/run/secrets/db_password"
-    pwFile  = open(pwFileLocation, "r")
+    pwFilePath = os.getenv("PW_FILE_PATH") or "/run/secrets/db_password"
+    pwFile = open(pwFilePath, "r")
     DB_PASSWORD = pwFile.readline().replace("\n", "")
     pwFile.close()
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
     DB_DATABASE_NAME = os.getenv("DB_DATABASE_NAME")
     try:
-        connection = psycopg2.connect(database = DB_DATABASE_NAME, 
-                            user = DB_USER, 
-                            host= DB_HOST,
-                            password = DB_PASSWORD,
-                            port = DB_PORT)
+        connection = psycopg2.connect(
+            database=DB_DATABASE_NAME,
+            user=DB_USER,
+            host=DB_HOST,
+            password=DB_PASSWORD,
+            port=DB_PORT,
+        )
         cursor = connection.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS occupancy(
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS occupancy(
                     timestamp timestamp PRIMARY KEY,
                     occupancy integer NOT NULL,
                     CONSTRAINT occ_valid CHECK (occupancy >= 0 AND occupancy <= 100));
-                    """)
-        cursor.execute(f"INSERT INTO occupancy(timestamp, occupancy) VALUES({timestamp},{occupancy})");
+                    """
+        )
+        cursor.execute(
+            f"INSERT INTO occupancy(timestamp, occupancy) VALUES({timestamp},{occupancy})"
+        )
         connection.commit()
     except:
-        logFile = open("/cron_task.log", "a")
-        logFile.write(f"Date: {timestamp}\t Error: could not write into DB, occupancy: {occupancy}\n")
+        logFile = open(logFilePath, "a")
+        logFile.write(
+            f"Date: {timestamp}\t Error: could not write into DB, occupancy: {occupancy}\n"
+        )
         logFile.close()
         connection.rollback()
         connection.close()
-    logFile = open("/cron_task.log", "a")
+    logFile = open(logFilePath, "a")
     logFile.write(f"Date: {timestamp}\t Occupancy: {occupancy}\n")
     logFile.close()
+
 
 source_code = loadSource()
 occupancy = getOccupancy(source_code)
